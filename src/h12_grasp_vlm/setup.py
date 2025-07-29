8f7259f3646dd55888661afeaeeec977655636d7
Matero952 <mateofeit@gmail.com>
2025-07-29 13:57:10 -0600

    redid ros_vision_pipeline_client a little bit and start dual arm client

--- src/h12_grasp_vlm/setup.py
+++ src/h12_grasp_vlm/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'ros_vision_pipe_client = h12_grasp_vlm.ros_vision_pipe_client:run_ros_vision_pipe_client'
         ],
     },
 )
