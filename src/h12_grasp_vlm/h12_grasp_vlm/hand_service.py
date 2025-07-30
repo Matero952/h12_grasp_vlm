85a0f278a15b29b0dc92c45db820f58a09036c0e
Matero952 <mateofeit@gmail.com>
2025-07-30 10:07:43 -0600

    started on hand controller server

--- src/h12_grasp_vlm/h12_grasp_vlm/hand_service.py
+++ src/h12_grasp_vlm/h12_grasp_vlm/hand_service.py
@@ -1,1 +1,17 @@
 #ok so for this hand service what do we wanna do?!
+import rclpy
+from rclpy.node import Node
+from custom_ros_messages.action import Hand
+from rclpy.action import ActionServer, CancelResponse
+
+class HandControllerServer(Node):
+    def __init__(self):
+        super().__init__('ROS_h12_controller_hand_controller_service')
+        self.hand_controller_service = ActionServer(
+            self,
+            Hand,
+            execute_callback=None,
+            cancel_callback=None,
+        )
+        #TODO MAKE THE CALLBACKS!!!!
+
