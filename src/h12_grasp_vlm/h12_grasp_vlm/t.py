32e0090d0f34fee0f018bc3f561f7cda8dc10a43
Matero952 <mateofeit@gmail.com>
2025-08-04 12:18:44 -0600

    oops turns out we dont need to crop lol

--- src/h12_grasp_vlm/h12_grasp_vlm/t.py
+++ src/h12_grasp_vlm/h12_grasp_vlm/t.py
@@ -5,8 +5,9 @@ from sensor_msgs.msg  import PointCloud2, PointField
 from std_msgs.msg import Header
 from sensor_msgs_py import point_cloud2
 from visualization_msgs.msg import Marker, MarkerArray
-from std_msgs.msg import ColorRGBA
+# from std_msgs.msg import ColorRGBA
 import time
+from geometry_msgs.msg._point import Point
 # import pandas as pd
 
 import struct
@@ -39,7 +40,7 @@ class MoveArmToPointCloud(Node):
     def marker_callback(self, msg):
         self.latest_marker = msg
         self.get_logger().info('Recieved marker array')
-        
+
     def get_point_cloud(self, timeout=5.0):
         start = time.time()
         while self.latest_pc is None and (time.time() - start < timeout):
@@ -60,7 +61,7 @@ class MoveArmToPointCloud(Node):
 
     def parse_pointcloud2_to_xyz_rgb(self, msg):
         num_points = msg.width * msg.height
-        print(f'{num_points=}')
+        # print(f'{num_points=}')
         # breakpoint()
         points_xyz_rgb = np.zeros((num_points, 6), dtype=np.float32)
         for i in range(num_points):
@@ -68,189 +69,143 @@ class MoveArmToPointCloud(Node):
             x = struct.unpack('f', msg.data[point_offset:point_offset+4])[0]
             y = struct.unpack('f', msg.data[point_offset+4:point_offset+8])[0]
             z = struct.unpack('f', msg.data[point_offset+8:point_offset+12])[0]
-            print(f'{x=}, {y=}, {z=}')
+            # print(f'{x=}, {y=}, {z=}')
             rgb_float = struct.unpack('f', msg.data[point_offset+12:point_offset+16])[0]
             rgb_int = struct.unpack('I', struct.pack('f', rgb_float))[0]
             r = (rgb_int >> 16) & 0xFF
             g = (rgb_int >> 8) & 0xFF  
             b = rgb_int & 0xFF
-            print(f'{r=}, {g=}, {b=}')
+            # print(f'{r=}, {g=}, {b=}')
             points_xyz_rgb[i] = [x, y, z, r, g, b]
             # breakpoint()
         return points_xyz_rgb
 
-    def get_unique_bbox_points(marker: Marker):
+    def get_unique_bbox_points(self, points: list[Point]):
         #the marker has repeated points bc of the way it defines 3d bboxes
         #and their edges, so this function gets the unique points from the 
         #geometry_msgs.msg.Point objects
+
         unique_points = set()
-        for p in marker.points:
+        for p in points:
             unique_points.add((p.x, p.y, p.z))
         print(f'{unique_points=}')
         print(f'{len(unique_points)=}')
         breakpoint()
         return unique_points
 
-    def get_best_3d_box(self, ma: MarkerArray):
+    def get_best_3d_box(self, ma: MarkerArray, debug=False):
         #this function gets the unique bbox points and then assembles it so that
         #we can filter point cloud points.
-        print(f'{ma.markers[0]}')
-        msg, _ = ma
-        unique_points = self.get_unique_bbox_points(msg.markers[0])
-        # breakpoint()
-        
-        # We know the 0th index will always have the best box?!
-        #ok so the markers are organized in alternating format where it goes prob marker, bounding box
-        # score_box_index = []
-        # #now we get best scores
-        # for idx, i  in enumerate(ma.markers):
-        #     if idx % 2 != 0:
-        #         # print(i)
-        #         # breakpoint()
-        #         text = str(i.text)
-        #         # print(text)
-        #         score = float(re.search(pattern = r'-?\d+\.?\d*', string=text).group(0))
-        #         # idx += 1
-        #         # print(score)
-        #         score_box_index.append([score, idx - 1])
-        
-        # # print(score_box_index)
-        # score_box_index = sorted(score_box_index, key=lambda x: x[0], reverse=True)
-        # best_box_index = score_box_index[0][1]
-        # #we sort all of our score box pairs and then get the index of the highest box
-        # best_box = ma.markers[best_box_index]
-        # # print(best_box)
-        # # print(len(best_box.points))
-        # # print(score_box_index)
-        # points = set()
-        # #line_list format requirees specification of start point and end point of each segment in our box, so we want to remove duplicates
-        # for p in best_box.points:
-        #     points.add((p.x, p.y, p.z))
-        # # print(len(points))
-        # # print(points)
-        # sorted(points, key=lambda x : (x[0], x[1], x[2]))
-
-        # return points
-        #make sure its sorted by x asacending, then y ascending, then z ascending
-
-    # def sort_point_cloud_points(self, point_cloud):
-        #ultimately, we want to basically crop the point cloud to jsut the points inside the box,
-        #so i want to do a binary search over the point lcoud points and sort them same way we do with the box points
-        # print(point_cloud)
-        
-        # print(len(point_cloud.data))
-        # print(self.parse_pointcloud2_to_xyz_rgb(point_cloud))
-        # point_cloud = self.parse_pointcloud2_to_xyz_rgb(point_cloud)
-        # sorted_point_cloud = sorted(point_cloud, key= lambda x: (x[0], x[1], x[2]))
-    #     marker_array = MarkerArray()
-    #     marker = Marker()
-    #     marker.header.frame_id = "map"  # Or your camera/robot/world frame
-    #     marker.header.stamp = self.get_clock().now().to_msg()
-    #     marker.ns = "bbox"
-    #     marker.id = 0
-    #     marker.type = Marker.CUBE
-    #     marker.action = Marker.ADD
-
-    #     # Set pose (center of the box)
-    #     marker.pose.position.x = 1.0
-    #     marker.pose.position.y = 2.0
-    #     marker.pose.position.z = 1.5
-
-    #     # Optionally, add rotation (quaternion)
-    #     marker.pose.orientation.w = 1.0
-
-    #     # Set size of the bounding box
-    #     marker.scale.x = 1.0  # width
-    #     marker.scale.y = 0.5  # height
-    #     marker.scale.z = 2.0  # depth
-
-    #     # Set color (RGBA)
-    #     marker.color = ColorRGBA(r=1.0, g=0.0, b=0.0, a=0.5)
-
-    #     marker.lifetime.sec = 0  # 0 = forever
-
-    #     marker_array.markers.append(marker)
-    # #     # print(sorted_point_cloud)
-    #     return marker_array
-        # return sorted_point_cloud
-
-
-    def crop_to_best_box(self, best_box, pc):
-        #requires that best_boox and pc are already sorted in x ascending, y ascending, and z ascending.
-        #I think we can first eliminate those that are outside x, and then those that are outside y, and then those that are outsize z
-        def get_upper_bound(pts, which_val: str, best_val):
-            assert which_val == 'x' or which_val == 'y' or which_val == 'z'
-            left = 0
-            right = len(pts)
-            if which_val == 'x':
-                idx = 0
-            elif which_val == 'y':
-                idx = 1
-            else:
-                idx = 2
-            while left < right:
-                mid = left + (right - left) // 2
-                if pts[mid][idx] <= best_val:
-                    left = mid + 1
-                else:
-                    right = mid
-            return left
-        
-        def get_lower_bound(pts, which_val:str, lowest_val):
-            assert which_val == 'x' or which_val == 'y' or which_val == 'z'
-            left = 0
-            right = len(pts)
-            if which_val == 'x':
-                idx = 0
-            elif which_val == 'y':
-                idx = 1
-            else:
-                idx = 2
-
-            while left < right:
-                mid = left + (right - left) // 2
-                if pts[mid][idx] < lowest_val:
-                    left = mid + 1
-                else:
-                    right = mid
-
-            return left
-
-        lower_x_indx = get_lower_bound(pc, 'x', list(best_box)[0][0])
-        upper_x_indx = get_upper_bound(pc, 'x', list(best_box)[len(best_box) - 1][0])
-
-        pc = pc[lower_x_indx: upper_x_indx]
-
-        lower_y_indx = get_lower_bound(pc, 'y', list(best_box)[0][1])
-        upper_y_indx = get_upper_bound(pc, 'y', list(best_box)[len(best_box) - 1][1])
-
-        pc = pc[lower_y_indx : upper_y_indx]
-
-        lower_z_indx = get_lower_bound(pc, 'z', list(best_box)[0][2])
-        upper_z_indx = get_upper_bound(pc, 'z', list(best_box)[len(best_box) - 1][2])
+        all_points = ma.markers[0].points
+        if debug:
+            print(f'\n\n\n{ma.markers[0]=}')
+            print(dir(Marker))
+            print(f'\n\n\n\n{ma.markers[0].points}')
+            print(len(ma.markers[0].points))
+            print(type(ma.markers[0].points))
+            # print(dir(Marker))
+            # breakpoint()
+            print(all_points)
+            print(len(all_points))
+            breakpoint()
+        unique_points = self.get_unique_bbox_points(all_points)
+        return unique_points
     
-        pc = pc[lower_z_indx : upper_y_indx]
+    def get_best_bbox_max_min_range(self, unique_points: set):
+        #gets the minimum and maximum points of each coordinate so that we can check if pc points
+        #are within the range
+        unique_points = list(unique_points)
+        foo_x, foo_y, foo_z = unique_points[0]
+        min_x, max_x, min_y, max_y, min_z, max_z = foo_x, foo_x, foo_y, foo_y, foo_z, foo_z
+        for p in unique_points:
+            x, y, z = p
+            if x < min_x:
+                min_x = x
+            elif x > max_x:
+                max_x = x
+            if y < min_y:
+                min_y = y
+            elif y > max_y:
+                max_y = y
+            if z < min_z:
+                min_z = z
+            elif z > max_z:
+                max_z = z
+        return {'min_x': min_x, 'max_x': max_x, 'min_y': min_y, 'max_y': max_y, 'min_z': min_z, 'max_z': max_z}
 
-        return pc
 
 
+    def crop_to_best_box(self, pc: np.ndarray, min_max_dict: dict):
+        x_min, x_max, y_min, y_max, z_min, z_max = min_max_dict.items()
+        # cropped_points_xyz_rgb = np.zeros((num_points, 6), dtype=np.float32)
+        print(f'len points before:{len(pc)}')
+        cropped_points = []
+        for p in pc:
+            x, y, z, _, _, _ = p
+            if x > x_min and x < x_max and y > y_min and y < y_max and z > z_min and z < z_max:
+                cropped_points.append(p)
+
+        print(f'len points after: {len(cropped_points)}')
+            
 
 
-                
+    # def crop_to_best_box(self, best_box, pc):
+    #     #requires that best_boox and pc are already sorted in x ascending, y ascending, and z ascending.
+    #     #I think we can first eliminate those that are outside x, and then those that are outside y, and then those that are outsize z
+    #     def get_upper_bound(pts, which_val: str, best_val):
+    #         assert which_val == 'x' or which_val == 'y' or which_val == 'z'
+    #         left = 0
+    #         right = len(pts)
+    #         if which_val == 'x':
+    #             idx = 0
+    #         elif which_val == 'y':
+    #             idx = 1
+    #         else:
+    #             idx = 2
+    #         while left < right:
+    #             mid = left + (right - left) // 2
+    #             if pts[mid][idx] <= best_val:
+    #                 left = mid + 1
+    #             else:
+    #                 right = mid
+    #         return left
+        
+    #     def get_lower_bound(pts, which_val:str, lowest_val):
+    #         assert which_val == 'x' or which_val == 'y' or which_val == 'z'
+    #         left = 0
+    #         right = len(pts)
+    #         if which_val == 'x':
+    #             idx = 0
+    #         elif which_val == 'y':
+    #             idx = 1
+    #         else:
+    #             idx = 2
 
-        #so we first want to cut down x points, then y points, then z points
-        #so we need to get lower x bound and then upper x bound, lower y bound and then upper y bound, and then lower z bound and then upper z bound
-        # while (left_x < right_x):
-        #     mid = left_x + (right_x - left_x) // 2
-        #     if pc[mid][0] < best_box[0].x:
-        #         #so if our point cloud x is less than the min x, then we move left to mid right?
-        #         left_x = mid + 1
-        #     elif pc[mid][0] > best_box[0].x:
-        #         #so if our pount cloud x is bigger than the min x, then we move right to mid
-        #         right_x = mid - 1
+    #         while left < right:
+    #             mid = left + (right - left) // 2
+    #             if pts[mid][idx] < lowest_val:
+    #                 left = mid + 1
+    #             else:
+    #                 right = mid
 
-        #     if pc[left_x][0] > best_box[0].x and pc[right_x][0] < best_box[len(best_box) - 1].x:
-        #         break
+    #         return left
+
+    #     lower_x_indx = get_lower_bound(pc, 'x', list(best_box)[0][0])
+    #     upper_x_indx = get_upper_bound(pc, 'x', list(best_box)[len(best_box) - 1][0])
+
+    #     pc = pc[lower_x_indx: upper_x_indx]
+
+    #     lower_y_indx = get_lower_bound(pc, 'y', list(best_box)[0][1])
+    #     upper_y_indx = get_upper_bound(pc, 'y', list(best_box)[len(best_box) - 1][1])
+
+    #     pc = pc[lower_y_indx : upper_y_indx]
+
+    #     lower_z_indx = get_lower_bound(pc, 'z', list(best_box)[0][2])
+    #     upper_z_indx = get_upper_bound(pc, 'z', list(best_box)[len(best_box) - 1][2])
+    
+    #     pc = pc[lower_z_indx : upper_y_indx]
+
+    #     return pc
         
     def create_cropped_cloud_ros_object(self, cropped_cloud):
         def rgb_to_float(r, g, b):
@@ -285,51 +240,31 @@ def main():
             # point_cloud = test_node.get_point_cloud()
             marker = test_node.get_marker()
             pc = test_node.get_point_cloud()
+
             if marker:
                 # with open('output2.txt', 'w') as f:
                 #     f.write(str(marker))
                 best_box = test_node.get_best_3d_box(marker)
+                best_box_max_min_dict = test_node.get_best_bbox_max_min_range(best_box)
+                print(best_box_max_min_dict)
                 breakpoint()
                 # print(f'{best_box=}')
                 # breakpoint()
             else: 
                 print("TIME OUT WAITING FOR MARKERE")
             if pc:
+                pc = test_node.parse_pointcloud2_to_xyz_rgb(pc)
+                print(pc[0:10])
                 # sorted_pc = test_node.sort_point_cloud_points(pc)
+                # pc
                 pass
             else:
                 print("TIME OUT WAITING FOR PC")
 
-            if pc and marker:
-                test_node.best_bbox_pub.publish(best_box)
-                # cropped_to_best_box = test_node.crop_to_best_box(best_box, sorted_pc)
-                # print(test_node.crop_to_best_box(best_box, sorted_pc))
-                # cropped_pc_message = test_node.create_cropped_cloud_ros_object(cropped_to_best_box)
-                # test_node.cropped_pc_pub.publish(cropped_pc_message)
-                # raise KeyboardInterrupt
-                # for i in marker.markers:
+            if pc is not None and marker is not None:
+                # test_node.best_bbox_pub.publish(best_box)
+                pass
 
-
-                #     print(type(i))
-                #     print(i.__str__)
-                #     breakpoint()
-                #     print(i._points)
-                #     print(len(i._points))
-                #     print((i._points))
-                #     # for j in i.
-                #     for j in i._points:
-                #         print(j.x)
-                #         print(j.y)
-                #         print(j.z)
-                #     breakpoint()
-
-                #     # for j in i.get_fields_and_field_types():
-                #     #     print(j['points'])
-                #     #     counter += 1
-
-                # # print(f"counter = {counter}")
-                # raise KeyboardInterrupt
-                # # limit -= 1
                 pass
             else:
                 print("Timed out waiting for point cloud.")
